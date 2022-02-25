1e76b0470856f71f5fa538f0abfc5e4ecd2bc27d
Kxffie <bryceriordan2005@gmail.com>
2022-02-24 21:46:51 -0600

    First Commit

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/navbar.module.scss'
+
+const Navbar = () => {
+    return (
+        <div className={styles.center}>
+            <div className={styles.title}>
+                Kxffiehub.xyz
+            </div>
+            <div className={styles.divider} />
+        </div>
+    )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Navbar from "../components/Navbar"
+import styles from '../styles/global.module.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.center}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
