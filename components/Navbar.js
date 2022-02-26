44bf4b7b8044c8ac55ff6a9ce310747c6492c3d2
Kxffie <bryceriordan2005@gmail.com>
2022-02-26 17:03:25 -0600

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import styles from '../styles/navbar.module.scss'
+import { GithubFill, DiscordFill, YoutubeFill, LinkChain } from 'akar-icons'
 
 const Navbar = () => {
     return (
         <div className={styles.center}>
             <div className={styles.title}>
-                Kxffiehub.xyz
+                Kxffiehub.xyz <a className={styles.copybtn} onClick={() =>  navigator.clipboard.writeText("https://emoticon.kxffiehub.xyz")} target="_blank" rel="noreferrer"><LinkChain strokeWidth={2} size={24} /></a>
+            </div>
+
+            <div className={styles.right}>
+                <a href="https://github.com/kxffie" target="_blank" rel="noreferrer"><GithubFill strokeWidth={2} size={24} /></a>
             </div>
             <div className={styles.right}>
-                <a href="https://github.com/kxffie" target="_blank" rel="noreferrer">github</a>
+                <a href="https://discord.gg/XhZEfYbMwE" target="_blank" rel="noreferrer"><DiscordFill strokeWidth={2} size={24} /></a>
             </div>
             <div className={styles.right}>
-                <a href="https://discord.gg/XhZEfYbMwE" target="_blank" rel="noreferrer">discord</a>
+                <a href="https://www.youtube.com/c/Kxffie" target="_blank" rel="noreferrer"><YoutubeFill strokeWidth={2} size={24} /></a>
             </div>
         </div>
     )
