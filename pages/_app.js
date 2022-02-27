9b810cf60a6df741f3f99bf588fac3d9e57d7595
Kxffie <bryceriordan2005@gmail.com>
2022-02-26 23:24:05 -0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,24 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer";
 import styles from '../styles/global.module.scss'
 
+import { useEffect } from "react";
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
 
   return (
     <div>
