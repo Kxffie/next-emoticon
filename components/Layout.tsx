879f8179a8d2e95db2fe2ec97c1723ac61b292a6
Kxffie <bryceriordan2005@gmail.com>
2022-03-02 17:53:44 -0600

    c

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Navbar } from './Navbar'
 
 export const Layout = ({children}) => {
     return (
         <div>
+            <div>
+                <Navbar />
+            </div>
             <div>
                 {children}
             </div>
