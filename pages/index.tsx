bad7d35b9dcea52d54e3b1e83fb6b895d05232de
Kxffie <bryceriordan2005@gmail.com>
2022-03-03 21:35:50 -0600

    c

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,19 +16,24 @@ export default function Home() {
         <meta name="keywords" content="HTML, CSS, JavaScript, NextJS, Next, ReactJS, React, Kxffie, Koffie, Youtube, Emoticon, Copy and Paste, Emoticon Website, Funny Emojis, Emoji, Emojis, Lenny, Kxffiehub, Xyz, Html, css, javascript, nextjs, next, reactjs, react, kxffie, koffie, youtube, emoticon, copy and paste, emoticon website, funny emojis, emoji, emojis, lenny, kxffiehub, xyz, html, css, javascript, nextjs, next, reactjs, react, kxffie, koffie, youtube, emoticon, copy and paste, emoticon website, funny emojis, emoji, emojis, lenny, kxffiehub, xyz, HTML, CSS, JAVASCRIPT, NEXTJS, NEXT, REACTJS, REACT, KXFFIE, KOFFIE, YOUTUBE, EMOTICON, COPY AND PASTE, EMOTICON WEBSITE, FUNNY EMOJIS, EMOJI, EMOJIS, LENNY, KXFFIEHUB, XYZ, emoticons, japanese emoticons, cute emoticons, kawaii emoticons, text emoticons, emoticons meaning, smiley emoticons, emoticons text, emoticons list, keyboard emoticons, facebook emoticons, fun emoticons, halloween emoticons, fun emoticons, happy emoticons, funny emoticons, cool emoticons, animated emoticons, emoticons heart, ascii emoticons, how to add emoticons to discord, how to get more steam emoticons, what are emoticons, what does the emoticons mean, how to make emoticons, how to use emoticons on facebook, what does mean emoticons, how to make an emoticons, how to type emoticons, how to turn off skype emoticons, how to add emoticons to text messages on android, how to do facebook emoticons, how to add emoticons on facebook, how to add emoticons to skype, how to use the new emoticons on facebook, how to get emoticons on iphone 5, how to make emoticons for twitch, arc, arc.io, arc io, arc io invite code, arc io alternative, arc io code, arc.io python liberary, arc.io invite, how does arc.io work" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9677093057091061" crossOrigin="anonymous"></script>
-        <script async src="https://arc.io/widget.min.js#DnCcJ8yZ"></script>
+        <script type="text/javascript">
+          window._mNHandle = window._mNHandle || {};
+          window._mNHandle.queue = window._mNHandle.queue || [];
+          medianet_versionId = "3121199";
+        </script>
+        <script src="https://contextual.media.net/dmedianet.js?cid=8CUH46574" async="async"></script>
       </Head>
 
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9677093057091061" crossOrigin="anonymous"></script>
-        {/* ads */}
-        <ins className="adsbygoogle"
-            style={{display: "block"}}
-            data-ad-client="ca-pub-9677093057091061"
-            data-ad-slot="6136901466"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-        <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>
+      <div id="173231350">
+        <script type="text/javascript">
+            try {
+              window._mNHandle.queue.push(function (){
+                window._mNDetails.loadTag("173231350", "160x600", "173231350");
+              });
+            }
+            catch (error) {}
+        </script>
+      </div>
 
       <main className={styles.main}>
         <div className={styles.ad}>
